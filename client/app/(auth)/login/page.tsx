10c25fa42d8e2546251fe223931f3cc8ccb05927
ashen <ashenprageeth1@gmail.com>
2023-07-10 10:49:05 +0530

    Halfly done login 0page

--- client/app/(auth)/login/page.tsx
+++ client/app/(auth)/login/page.tsx
@@ -1,7 +1,46 @@
+import { buttonVariants } from '@/components/UI/button';
 import React from 'react'
 
-export default function Login() {
-  return (
-    <div>login page</div>
-  )
-}
+
+
+
+
+function LoginPage() {
+    return (
+     
+
+         <div className='grid grid-cols-2 h-screen '>
+            <div className='bg-slate-900 top-1'>
+
+            </div>
+
+            <div className="flex flex-col items-center py-4 gap-2 text-accent/60">
+               
+              
+            <div className="flex flex-row items-center mt-60">
+                <h1 className="text-3xl font-bold">Welcome</h1>
+                 <h1 className="ml-2 font-medium text mt-2 "> back, log in here!</h1>
+            </div>
+            <div className="w-80 h-96 opacity-25 bg-gray-200 rounded-2xl">
+            </div>
+
+            <button className={`${buttonVariants()}`}>
+                asdada
+                
+            </button>
+
+
+             </div>
+             
+            
+         </div>
+  
+            
+           
+            
+            
+        
+    );
+  }
+  
+  export default LoginPage;
--- client/app/(auth)/register/page.tsx
+++ client/app/(auth)/register/page.tsx
@@ -1,5 +1,8 @@
 function Register() {
   return <main></main>;
+
+
+  
 }
 
 export default Register;
